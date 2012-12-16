70ce993686fccfe64e6813f049731041799d71e5
Asger Hallas <ahl@asgerhallas.dk>
2012-12-16 11:44:26 +0100

    Add the FK project

---
+++ src/Rebus.FleetKeeper.Tryouts/Program.cs
@@ -0,0 +1,18 @@
+﻿using Rebus.Configuration;
+using Rebus.Transports.Msmq;
+
+namespace Rebus.FleetKeeper.Tryouts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = Configure.With(new BuiltinContainerAdapter())
+                               .Transport(x => x.UseMsmq("fleetkeeper", "fleetkeeper.errors"))
+                               .InstallFleetKeeper()
+                               .CreateBus()
+                               .Start();
+
+        }
+    }
+}
---
+++ src/Rebus.FleetKeeper/Program.cs
@@ -0,0 +1,19 @@
+﻿using Rebus.Configuration;
+
+namespace Rebus.FleetKeeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public static class BusExtensions
+    {
+        public static RebusConfigurer InstallFleetKeeper(this RebusConfigurer configurer)
+        {
+            return configurer;
+        }
+    }
+}
